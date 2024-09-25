117b8d86971e088ca00be56eeb6269c0160d1201
LoganJeong0 <loganjeong0@gmail.com>
2024-09-25 16:17:14 +0000

    first commit

---
+++ Scoreboard.java
@@ -0,0 +1,14 @@
+public class Scoreboard
+{
+    public Scoreboard(String one, String two)
+    {
+        teamOne = one;
+        teamTwo = two;
+    }
+        public String getScore()
+    {
+        return null;
+    }
+
+
+}
